e410b19fadd1036ba7a734ec1b74839da20ff14e
hendrix.0 <87786189+hendriox0@users.noreply.github.com>
2025-03-17 22:08:40 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+let circulo = document.querySelector(".circulo")
+let img = document.querySelector(".starbucks");
+
+
+function trocarcor(cor){
+    circulo.style.background = cor
+}
+
+function trocaImagem(imagem){
+    img.src = imagem
+}
